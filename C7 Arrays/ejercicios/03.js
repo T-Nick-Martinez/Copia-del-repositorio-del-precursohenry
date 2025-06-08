113342a5f7fe7d790b175c34c70414fe9d8108ef
tomin <martinez.t.nick.707@gmail.com>
2025-06-08 17:24:28 -0300

    .

--- C7 Arrays/ejercicios/03.js
+++ C7 Arrays/ejercicios/03.js
@@ -1,6 +1,8 @@
 function obtenerLargoDelArray(array) {
   // Retornar la longitud del arreglo recibido.
   // Tu código:
+  var long= array.length;
+  return long;
 }
 
 module.exports = obtenerLargoDelArray;
--- C7 Arrays/ejercicios/06.js
+++ C7 Arrays/ejercicios/06.js
@@ -3,4 +3,21 @@ function invertirArray(array) {
   // Tu código:
 }
 
+var array=["comida", "documentos", "ropa", "juguetes", "higiene"];
+var lastof = [];
+console.log(array);
+//console.log (lastof);
+
+var long= (array.length)-1;
+var i=0
+do {
+  lastof= [lastof + " " + (array.pop())]
+  i= i +1;
+  console.log(lastof);
+
+}
+while(i<=long)
+  var arregloinvertido = lastof.split(" ");
+  console.log(arregloinvertido)
+  console.log(lastof);
 module.exports = invertirArray;
